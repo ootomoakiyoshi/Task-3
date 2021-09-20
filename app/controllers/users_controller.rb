5fd291948e2ef7346835e0696437a28b359e6242
ootomoakiyoshi <ootomoakiyoshi@icloud.com>
2021-09-20 10:58:51 +0000

    ファイルの設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,24 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    @user.user_id = current_user.id
+    @user.save
+    redirect_to user_path
+  end
+
+  def index
+    @user = user.all
+  end
+private
+
+  def post_user_params
+    params.require(:user).permit(:shop_name, :image, :caption)
+  end
 end
