9bd7300dfe368d61ac7d62a19a9267c84c853cda
ootomoakiyoshi <ootomoakiyoshi@icloud.com>
2021-09-23 11:30:11 +0000

    editpege

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,21 +6,40 @@ class BooksController < ApplicationController
 
   def create
     @book = Book.new(book_params)
-   if @book.save
+    @book.user_id = current_user.id
+    if
+     @book.save
       flash[:notice] = "Book was successfully created." #フラッシュ文リダイレクと前に
       redirect_to book_path(@book.id)
-   else
+    else
       @books=Book.all
       render :index
-   end
+    end
   end
 
   def index
     @books = Book.all
+    @book = Book.new
+    @user = current_user
   end
 
-  # def show
-  # end
+   def show
+    @users = User.all
+    @newbook = Book.new
+    @user = current_user
+    @book = Book.find(params[:id])
+   end
+
+   def edit
+    @book = Book.find(params[:id])
+    @user = current_user
+   end
+
+   def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@user.id)
+   end
 
   # def destroy
   # end
@@ -28,6 +47,6 @@ class BooksController < ApplicationController
 
  private
   def book_params
-    params.require(:book).permit(:title,:body)
+    params.require(:book).permit(:title, :body)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,34 +1,32 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @books = Book.all
+    @book = Book.new
+    @user = current_user
   end
 
   def new
     @user = User.new
   end
 
-  def create
-    @user = User.new(user_params)
-    @user.user_id = current_user.id
-    @user.save
-    redirect_to user_path
-  end
 
   def edit
     @user = User.find(params[:id])
-
   end
 
   def update
     @user = User.find(params[:id])
     @user.update(user_params)
     redirect_to users_path
+    
   end
 
 
   def index
     @user = current_user
     @users = User.all
+    @book = Book.new
   end
 
 private
